f06ca924040f8507c01d95e68379d0aa43f08187
VO THANH TUNG <vo_vietis@vo-vietis1.local>
2017-01-17 18:32:17 +0900

    add mongo as db

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
-import test from '../imports/components/test/test';
+import testname from '../imports/components/test/test';
  
 angular.module('metorapp', [
   angularMeteor,
   todosList.name,
-  test.name,
+  testname.name,
 ]);
--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,16 +1,16 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
+import { Tasks } from '../../api/tasks.js';
  
 class Abc {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
   }
 }
  // export to template which is used on main html 
@@ -19,5 +19,7 @@ export default angular.module('todos', [
 ])
   .component('todos', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: Abc
+    controller: ['$scope', Abc]
   });
+  // o day no add Abc vao todos 
+  // con no lam gi minh van chua ro 
