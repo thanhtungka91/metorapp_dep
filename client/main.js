7d6e5c6d59242b34a5dc65177ddb75a55f11c8e2
VO THANH TUNG <vo_vietis@vo-vietis1.local>
2017-01-23 14:34:37 +0900

    add account user module

--- client/main.js
+++ client/main.js
@@ -2,9 +2,11 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
 import testname from '../imports/components/test/test';
+import '../imports/startup/accounts-config.js';
  
 angular.module('metorapp', [
   angularMeteor,
   todosList.name,
   testname.name,
+  'accounts.ui'
 ]);
